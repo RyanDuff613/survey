6b3719826af5a19b1224a5ddd9ea9d8725fe3f08
Ryan Duff <ryanduff613@gmail.com>
2020-03-25 16:11:39 -0700

    add all html forms, add javascript reteurning value from each

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+  
+  var beverage = $("#beverage").val();
+
+  var flavor = $("input:radio[name=flavor]:checked").val();
+
+  var dob = $("#born").val();
+
+  var favoriteColor = $("#color").val();
 
 });
